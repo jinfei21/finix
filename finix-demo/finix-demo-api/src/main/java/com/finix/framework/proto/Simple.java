f3b61c89ef9d317d4615f9b96119dffd03722f27
feiyongjun <63581560@qq.com>
2018-01-08 16:55:55 +0800

    add demo

---
+++ finix-demo/finix-demo-api/src/main/java/com/finix/framework/proto/Simple.java
@@ -0,0 +1,10 @@
+package com.finix.framework.proto;
+
+import com.finix.framework.annotation.FinixInterface;
+
+@FinixInterface
+public interface Simple {
+
+    Helloworld.HelloReply sayHello(Helloworld.HelloRequest request);
+
+}
